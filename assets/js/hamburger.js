497258de4145fd055cc40f37c33c82de14e24e2f
jamoliddin05 <jamoliddin05@users.noreply.github.com>
2024-09-03 11:10:52 +0000

    deploy: d62f8f6a1b3529d3b4ae54dc88dc0deb24c7a536

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("hamburger-button").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open");
         document.querySelector("main").classList.toggle("overlay");
-        document.querySelector(".carousel-caption-custom").classList.toggle("slider-overlay");
-        document.querySelector(".carousel-image").classList.toggle("slider-overlay");
+        document.querySelector(".dropdown-custom").classList.toggle("open");
     })
 })
