5ef42efa6f94f1e352acb1fbf811393d4aff1ea4
jamoliddin05 <jamoliddin05@users.noreply.github.com>
2024-09-04 07:29:33 +0000

    deploy: b048d17605d8c0f7d62902f04b4f6926f3553ef8

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -2,16 +2,25 @@
 const lightbox = document.getElementById('lightbox');
 const lightboxContent = document.getElementById('lightbox-content');
 const lightboxClose = document.getElementById('lightbox-close');
-const triggers = document.querySelectorAll('.lightbox-trigger');
+const triggers = document.querySelectorAll('.image-block img');
 
 // Function to open the lightbox
 function openLightbox(src) {
     lightbox.style.display = 'flex';
+    lightbox.classList.remove('lightbox-close-animation');
     lightboxContent.src = src;
+    // Use a timeout to add the animation class after assigning the src
+    setTimeout(() => {
+        lightboxContent.classList.add('lightbox-animation');
+        lightboxClose.classList.add('lightbox-animation');
+    }, 10); // Delay for 50 milliseconds (adjust as needed)
 }
 
 // Function to close the lightbox
 function closeLightbox() {
+    setTimeout(() => {
+        lightboxContent.classList.remove('lightbox-animation');
+    }, 100);
     lightbox.style.display = 'none';
     lightboxContent.src = '';
 }
