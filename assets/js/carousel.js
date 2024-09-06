84d041310620d60d5159a1974428830207451dea
jamoliddin <jamol05@icloud.com>
2024-09-06 11:42:57 +0500

    Demo ready

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,10 +2,7 @@ var elem = document.querySelector('.carousel-bottom');
 var flkty = new Flickity( elem, {
     // options
     wrapAround: true,
-    freeScroll: false,
+    freeScroll: true,
     lazyLoad: 4,
     pageDots: false,
-    selectedAttraction: 0.1,
-    friction: 0.8,
-    dragThreshold: 50,
 });
