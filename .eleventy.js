e85a957ba28b3f2c3a4f5f167a30943cc8140fb3
jamoliddin <jamol05@icloud.com>
2024-09-07 12:01:06 +0500

    Added a CNAME file

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("CNAME");
 
     // Return your Object options:
     return {}
