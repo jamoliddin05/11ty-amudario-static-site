e411f4a3443efa611b2a3b2b685522439703d640
jamoliddin05 <jamoliddin05@users.noreply.github.com>
2024-10-03 04:07:32 +0000

    deploy: a134ca6de82c671c3c1c62221630c3bb6e2b5e4b

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,7 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const dropdown = document.querySelector(".dropdown-content");
+
     document.getElementById("hamburger-button").addEventListener("click", function() {
         document.querySelector("header").classList.toggle("open");
-        document.querySelector("main").classList.toggle("overlay");
+        // document.querySelector("main").classList.toggle("overlay");
         document.querySelector(".dropdown-custom").classList.toggle("open");
     })
+
+    dropdown.addEventListener('click', function(event) {
+        document.querySelector("header").classList.toggle("open");
+        document.querySelector(".dropdown-custom").classList.toggle("open");
+    })
+
+    document.addEventListener('click', function(event) {
+        // Check if the click is outside the dropdown
+        // if (!dropdown.contains(event.target)) {
+        //     document.querySelector("header").classList.toggle("open");
+        //     document.querySelector(".dropdown-custom").classList.toggle("open");
+        // }
+    });
 })
