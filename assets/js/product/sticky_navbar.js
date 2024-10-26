0bc8c716c7745c71e748e0d4d33d16f5abcabd2f
jamoliddin05 <jamoliddin05@users.noreply.github.com>
2024-10-26 11:31:24 +0000

    deploy: 4335dc70650b82fb5ac9c50a304cf73c3bceaec0

--- assets/js/product/sticky_navbar.js
+++ assets/js/product/sticky_navbar.js
@@ -12,26 +12,24 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const dropdownLink = document.getElementById('product-dropdown');
-//     const dropdownWrapper = document.querySelector('.dropdown-product-wrapper');
-//     const dropDown = document.querySelector('.product-dropdown-menu');
-//
-//     // Toggle 'active' class when the link is clicked
-//     dropdownLink.addEventListener('click', function(event) {
-//         event.preventDefault();
-//         dropdownWrapper.classList.toggle('active');
-//     });
-//
-//     // Remove 'active' class when clicking outside the dropdown
-//     document.addEventListener('click', function(event) {
-//         // Check if the click is outside the dropdown
-//         if (!dropdownWrapper.contains(event.target)) {
-//             dropdownWrapper.classList.remove('active');
-//         }
-//     });
-//
-//     dropDown.addEventListener('click', function(event) {
-//         dropdownWrapper.classList.remove('active');
-//     });
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    const languageDropdown = document.getElementById('language-dropdown');
+    const dropdownLangWrapper = document.querySelector('.dropdown-lang-wrapper');
+    const dropDownLang = document.querySelector('.lang-dropdown-menu');
+
+    languageDropdown.addEventListener('click', function(event) {
+        event.preventDefault();
+        dropdownLangWrapper.classList.toggle('active');
+    });
+
+    document.addEventListener('click', function(event) {
+        // Check if the click is outside the dropdown
+        if (!dropdownLangWrapper.contains(event.target)) {
+            dropdownLangWrapper.classList.remove('active');
+        }
+    });
+
+    dropDownLang.addEventListener('click', function(event) {
+        dropDownLang.classList.remove('active');
+    });
+});
--- assets/js/sticky_navbar.js
+++ assets/js/sticky_navbar.js
@@ -14,13 +14,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
     const dropdownLink = document.getElementById('product-dropdown');
+    const languageDropdown = document.getElementById('language-dropdown');
     const dropdownWrapper = document.querySelector('.dropdown-product-wrapper');
+    const dropdownLangWrapper = document.querySelector('.dropdown-lang-wrapper');
     const dropDown = document.querySelector('.product-dropdown-menu');
+    const dropDownLang = document.querySelector('.lang-dropdown-menu');
 
     // Toggle 'active' class when the link is clicked
     dropdownLink.addEventListener('click', function(event) {
         event.preventDefault();
         dropdownWrapper.classList.toggle('active');
+    });
+
+    languageDropdown.addEventListener('click', function(event) {
+        event.preventDefault();
+        dropdownLangWrapper.classList.toggle('active');
     });
 
     // Remove 'active' class when clicking outside the dropdown
@@ -31,7 +39,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    document.addEventListener('click', function(event) {
+        // Check if the click is outside the dropdown
+        if (!dropdownLangWrapper.contains(event.target)) {
+            dropdownLangWrapper.classList.remove('active');
+        }
+    });
+
     dropDown.addEventListener('click', function(event) {
         dropdownWrapper.classList.remove('active');
+    });
+
+    dropDownLang.addEventListener('click', function(event) {
+        dropDownLang.classList.remove('active');
     });
 });
