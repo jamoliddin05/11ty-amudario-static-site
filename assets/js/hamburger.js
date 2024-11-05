2a0b5864d792889e80b7a41994368f568cbdc3fe
jamoliddin <jamol05@icloud.com>
2024-11-05 21:17:52 +0500

    Hello

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -2,21 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const dropdown = document.querySelector(".dropdown-content");
 
     document.getElementById("hamburger-button").addEventListener("click", function() {
-        document.querySelector("header").classList.toggle("open");
-        // document.querySelector("main").classList.toggle("overlay");
-        document.querySelector(".dropdown-custom").classList.toggle("open");
+        document.querySelector("header").classList.add("open");
+        // document.querySelector(".dropdown-custom").classList.toggle("open");
     })
 
     dropdown.addEventListener('click', function(event) {
-        document.querySelector("header").classList.toggle("open");
-        document.querySelector(".dropdown-custom").classList.toggle("open");
+        document.querySelector("header").classList.remove("open");
+        // document.querySelector(".dropdown-custom").classList.toggle("open");
     })
 
     document.addEventListener('click', function(event) {
-        // Check if the click is outside the dropdown
-        // if (!dropdown.contains(event.target)) {
-        //     document.querySelector("header").classList.toggle("open");
-        //     document.querySelector(".dropdown-custom").classList.toggle("open");
-        // }
+        if (event.target !== document.getElementById("hamburger-button")) {
+            document.querySelector("header").classList.remove("open");
+        }
     });
 })
