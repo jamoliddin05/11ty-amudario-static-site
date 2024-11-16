ef47594a98fb014d2095dd4b7fbf9a9c3a29307a
jamoliddin05 <jamoliddin05@users.noreply.github.com>
2024-11-16 04:51:13 +0000

    deploy: 6469c85a8127acfa116fe82487487061476ec75e

---
+++ assets/js/partners.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.carousel-partners');
+var flkty = new Flickity( elem, {
+    // options
+    wrapAround: true,
+    freeScroll: true,
+    lazyLoad: 3,
+    pageDots: false,
+});
